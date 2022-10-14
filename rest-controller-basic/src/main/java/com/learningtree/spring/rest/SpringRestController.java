7be22024924915a1b56f115da306daa80bc1dddf
Ari Meyer <ari.meyer@gmail.com>
2022-10-14 15:53:46 -0400

    Added comments to basic SprintRestController

--- rest-controller-basic/src/main/java/com/learningtree/spring/rest/SpringRestController.java
+++ rest-controller-basic/src/main/java/com/learningtree/spring/rest/SpringRestController.java
@@ -16,37 +16,37 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Transactional
-@RequestMapping("/users")
+@RequestMapping("/users")  // GET http://localhost:8080/users/name/ernie
 public class SpringRestController {
 	@Autowired
 	private UserJpaRepository repository;
 	
-	@GetMapping("/all")
+	@GetMapping("/all")  // GET http://localhost:8080/users/all
 	public List<User> getAllUsers() {
 		return repository.findAll(); 
 	}
 	
-	@GetMapping("/{id}")
+	@GetMapping("/{id}")  // GET http://localhost:8080/users/ernie  
 	public User getUserById(@PathVariable String id) {
 		return repository.findById(id).get(); 
 	}
 	
-	@GetMapping("/name/{userName}")
+	@GetMapping("/name/{userName}")  // GET http://localhost:8080/users/name/ernie
 	public User getUserByUserName(@PathVariable String userName) {
 		return repository.findByUserName(userName).get(); 
 	}
 	
-	@PostMapping
+	@PostMapping  // POST http://localhost:8080/users
 	public User createUser(@RequestBody User user) {
 		return repository.save(user); 
 	}
 	
-	@PutMapping
+	@PutMapping   // PUT http://localhost:8080/users
 	public User updateUser(@RequestBody User user) {
 		return repository.save(user); 
 	}
 	
-	@DeleteMapping("/{id}")
+	@DeleteMapping("/{id}")  // DELETE http://localhost:8080/users/bigbird
 	public void deleteUser(@PathVariable String id) {
 		repository.deleteById(id); 
 	}
